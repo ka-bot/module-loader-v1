4f2878cc991716316c19a718427b5c0490d5104c
맹주영 <mjy9088@naver.com>
2019-01-09 23:20:44 +0900

    reflection을 사용하도록 변경 (희망고문)

--- response.js
+++ response.js
@@ -17,7 +17,7 @@ function loadModule(path)
 	try
 	{
 		let e = "Invalid v1g module or I/O exception occurred";
-		var zf = new java.util.ZipFile(path);
+		var zf = java.lang.Class.forName("java.util.ZipFile").getConstructor([java.lang.String]).newInstance([path]);
 		var module_json = getEntry("module.json");
 		if(module_json == null || module_json.isDirectory())
 		{
