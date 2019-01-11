469d3363f500b0b9130e73d93011bcd1d209c946
맹주영 <mjy9088@naver.com>
2019-01-11 19:57:21 +0900

    오류 수정

--- response.js
+++ response.js
@@ -5,7 +5,7 @@ function readAll(inputStream)
 {
 	let len = inputStream.available();
 	let buf = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, len);
-	if(fis.read(buf) != len)
+	if(inputStream.read(buf) != len)
 	{
 		throw "Failed to read all";
 	}
@@ -18,7 +18,7 @@ function loadModule(path)
 	{
 		let e = "Invalid v1g module or I/O exception occurred";
 		var zf = new java.util.zip.ZipFile(path);
-		var module_json = getEntry("module.json");
+		var module_json = zf.getEntry("module.json");
 		if(module_json == null || module_json.isDirectory())
 		{
 			throw e;
@@ -51,7 +51,7 @@ function loadModule(path)
 		{
 			throw e;
 		}
-		var script = getEntry(module.filename);
+		var script = zf.getEntry(module.filename);
 		var func = new Function("shared", readAll(zis = script.getInputStream()));
 		zis.close();
 		zis = undefined;
